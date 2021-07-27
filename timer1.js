3f3a3c466fa1e5a0149f1fb8d5b782d2530d57e9
GledfullyChill <stephan.lofstrom@gmail.com>
2021-07-27 15:54:37 -0600

    working timer

--- timer1.js
+++ timer1.js
@@ -1,4 +1,10 @@
 let args = process.argv.slice(2);
+const exec = require('child_process').execSync;
 for(let i =0; i < args.length; i++) {
-  
-}
+  args[i] = args[i] * 1000;
+  if (typeof(args[i]) === 'number' && args[i] > 0) {
+    setTimeout(() => {
+      exec('command say beep')
+    }, args[i])
+  }
+};
